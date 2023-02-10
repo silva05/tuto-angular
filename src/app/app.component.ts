f0480b850a56f57a2219bc0c57d7ce417e018560
OGTIC 18 INDECI <ogtic_18@INDECI.GOB.PE>
2023-02-10 17:42:58 -0500

    output e input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
   //link de imagen
   imgParent= 'https://www.nationalgeographic.com.es/medio/2023/01/12/imagen-de-un-atomo_952cc086_230112124807_1280x988.jpg';
 
-  onLoaded(){
-    console.log('log padre');
+  loadpadre(img : String ){
+    console.log('log padre',img);
   }
 
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,5 @@
-import { Input, Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Input, Component,
+   OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -8,21 +9,17 @@ import { Input, Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class ImgComponent implements OnInit {
 
   @Input() img: string = ''
-  imageDef = 'https://www.nationalgeographic.com.es/medio/2023/01/12/imagen-de-un-atomo_952cc086_230112124807_1280x988.jpg'
-  @Output() loaded = new EventEmitter ();
+
+  imgdefecto = 'https://www.softzone.es/app/uploads/2018/04/guest.png'
+  @Output() loaded = new EventEmitter <string>();
 
   constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
-  }
+  imgError(){ this.img=this.imgdefecto; }
 
-  imgError(){
-    this.img=this.imageDef;
-  }
-
-  imgloaded(){
+  imgloadhijo(){ //Manda el mensaje al componente padre
     console.log("load hijo");
-    this.loaded.emit()
+    this.loaded.emit(this.img)
   }
-
 }
